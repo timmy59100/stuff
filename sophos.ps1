668908a5d8144282cb9a2347bb0165127deadadf
alu <adi_karate@hotmail.com>
2019-06-12 15:58:24 +0200

    functioning

--- sophos.ps1
+++ sophos.ps1
@@ -1,26 +1,50 @@
-$password = "asdf" | ConvertTo-SecureString -asPlainText -Force
-$username = "root" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-$session = New-SSHSession -ComputerName 192.168.0.1 -KeyFile "C:\Users\Adrian\Desktop\sophosprivate.key" -Credential $credential
-$ssh = Invoke-SSHCommand -Index 0 -Command "/usr/local/bin/confd-client.plx get_ipsec_status|grep 'REF_\|all_established'"
 
+$password = "asdf" | ConvertTo-SecureString -asPlainText -Force
+$username = "root"
+$keyfile = "C:\Users\Administrator\Desktop\sophosprivate.key"
+$command = "/usr/local/bin/confd-client.plx get_ipsec_status|grep 'REF_\|all_established'"
+
+
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+$session = New-SSHSession -ComputerName 192.168.246.1 -KeyFile $keyfile -Credential $credential
+$ssh = Invoke-SSHCommand -Index 0 -Command $command
+
+
+function add-Channel {
+    param (
+        $channelname,
+        $channelvalue
+    )
+    if($channelvalue -eq 0){$warning = 1}else{$warning = 0}
+
+    $channel = "
+    <result>
+        <channel>" + $channelname + "</channel>
+    " + "   <unit>Status</unit>
+        <value>" + $channelvalue + "</value>
+        <ValueLookup>prtg.standardlookups.boolean.statetrueok</ValueLookup>
+    </result>"
+
+    $channel
+}
+$prtg = "<prtg>"
 
 $counter = 0
 foreach($string in $ssh.output)
     {
-        if ($string -contains "0") {
+        if ($string -match '0') {
             $result = 0
         }
-        elseif ($string -contains "1") {
+        elseif ($string -match '1') {
             $result = 1
         }
         else {
-            $result = $string.replace("'REF_IpsS", '') | $_.replace("", '')
+            $result = $string.replace("          'REF_IpsSit", '')
+            $result = $result.replace("' => {", '')
         }
         if($counter % 2) {
         $vpn_status = $result
-        $prtg+= $vpn_name + $vpn_status
-        #prtg.add_channel(channel_name=vpn_name, unit="VPN Status", value=vpn_status)
+        $prtg += add-Channel -channelname $vpn_name -channelvalue $vpn_status
         }
         else{
         $vpn_name = $result
@@ -28,4 +52,8 @@ foreach($string in $ssh.output)
         $counter++
     }
 
-Write-Host $prtg
+$prtg += "
+</prtg>"
+Remove-Item "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\data.txt"
+
+$prtg >> "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\data.txt"
